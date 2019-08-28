46bc44cf827770d5466ab5fdf7a7bb48127ac8df
Mariusz Mazyrko <m-mazyrko@gmail.com>
2019-08-28 22:44:30 +0200

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,49 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
+
+const number = document.querySelector('.hydro__box__number--js');
+const addGlass = document.querySelector('.button__cont--add');
+const removeGlass = document.querySelector('.button__cont--remove--js');
+const removeAll = document.querySelector('.button__cont--remove--all--js')
+const summary = document.querySelector('.result__info--js');
+const key = new Date().toISOString().slice(0, 10);
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+}
+
+console.log(number, addGlass, removeGlass, removeAll);
+
+addGlass.addEventListener('click', () => {
+
+  if (number.innerHTML <= 0) {
+    number.innerHTML++
+    localStorage.setItem(key, number.innerHTML);
+    summary.innerHTML = (`${key} You drank ${localStorage.getItem(key, number.innerHTML)} glasses of water today`);
+  } else if (number.innerHTML >= 1) {
+    number.innerHTML++
+    localStorage.setItem(key, number.innerHTML);
+    summary.innerHTML = (`You drank ${key} ${localStorage.getItem(key, number.innerHTML)} glasses of water today`);
+  }
+});
+
+
+removeGlass.addEventListener('click', () => {
+  if (number.innerHTML > 2) {
+    number.innerHTML--
+    localStorage.setItem(key, number.innerHTML);
+    summary.innerHTML = (`${key} You drank ${localStorage.getItem(key, number.innerHTML)} glasses of water today`);
+  } else if (number.innerHTML <= 2 && number.innerHTML > 0) {
+    number.innerHTML--
+    localStorage.setItem(key, number.innerHTML);
+    summary.innerHTML = (`${key} You drank ${localStorage.getItem(key, number.innerHTML)} glasses of water today`);
+    console.log("else if")
+  }
+});
+
+removeAll.addEventListener('click', () => {
+  number.innerHTML = '0';
+  localStorage.setItem(key, 0);
+  summary.innerHTML = 'You drank 0 glasses of water today! Go now and drink some!';
+});
